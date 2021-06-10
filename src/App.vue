f8e261a9789c5d57f1770dd57a623a98c6218d19
yejiyuan <1286412801@qq.com>
2021-06-10 17:57:35 +0200

    持久化token

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,25 @@ import navbar from "@/components/Navbar.vue";
     navbar,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  created() {
+    //在页面加载时读取sessionStorage里的状态信息
+    if (localStorage.getItem("store")) {
+      this.$store.replaceState(
+        Object.assign(
+          {},
+          this.$store.state,
+          JSON.parse(localStorage.getItem("store"))
+        )
+      );
+    }
+
+    //在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener("beforeunload", () => {
+      localStorage.setItem("store", JSON.stringify(this.$store.state));
+    });
+  }
+}
 </script>
 <style lang="scss">
 #app {
